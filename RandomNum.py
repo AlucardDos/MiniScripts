0f9f95911f02080681ddd3ff05929b23c006f9d4
AlucardDos <47983256+AlucardDos@users.noreply.github.com>
2020-01-12 10:58:53 +0200

    Add files via upload
    Генерирует рандомное число
    Можно добавить число в список исключений, чтобы не происходила его генерация при выполнении

---
+++ RandomNum.py
@@ -0,0 +1,23 @@
+import random
+k=[0]
+def rnum(a,b):
+   i = random.randit(a,b)
+   return i
+c = input("1.Сгенерировать число \n 2.Добавить исключение \n 3.Удалить все исключения")
+if c==2:
+   k.append(input("Число: "))
+if c==3:
+   k.clear()
+if c==1:
+   print("Генерация случайного числа в диапазоне")
+   a = input("От: ")
+   b = input("До: ")
+   j = rnum(a,b)
+   while j in k:
+      j = rnum(a,b)
+   else:
+      print(j)
+      k.append(j)
+
+
+   
