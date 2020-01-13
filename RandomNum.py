1501080d3e1bd8ab4fb3e284705cd8d0dff0c755
AlucardDos <47983256+AlucardDos@users.noreply.github.com>
2020-01-13 16:27:07 +0200

    Add files via upload

--- RandomNum.py
+++ RandomNum.py
@@ -1,23 +1,32 @@
 import random
-k=[0]
-def rnum(a,b):
-   i = random.randit(a,b)
-   return i
-c = input("1.Сгенерировать число \n 2.Добавить исключение \n 3.Удалить все исключения\n")
-if c==2:
-   k.append(input("Число: "))
-if c==3:
-   k.clear()
-if c==1:
-   print("Генерация случайного числа в диапазоне")
-   a = input("От: ")
-   b = input("До: ")
-   j = rnum(a,b)
-   while j in k:
-      j = rnum(a,b)
-   else:
-      print(j)
-      k.append(j)
-
-
-   
+l = True
+while l:
+   k=[0]
+   def rnum(a,b):
+      i = random.randint(a,b)
+      return i
+   print("1.Сгенерировать число")
+   print("2.Добавить исключение")
+   print("3.Удалить все исключения")
+   print("4.Выход")
+   c =int( input())
+   if c==1:
+      print("Генерация случайного числа в диапазоне")
+      a =int(input("От: "))
+      b = int(input("До: "))
+      j =int( rnum(a,b))
+      x=True
+      while x:
+      	if j in k:
+      		j= rnum(a,b)
+      	else:
+      		x=False
+      print("Число: " + j)
+   if c==4:
+      	l=False
+   if c==2:
+   	k.append(input("Число: "))
+   if c==3:
+   	k.clear()
+   if c==4:
+   	l = False
